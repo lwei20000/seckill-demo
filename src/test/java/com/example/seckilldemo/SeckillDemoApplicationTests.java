621f307db86e586e6cbe3b9186e4125e3a4fc0b6
“lw20000” <1291958264@qq.com>
2022-06-20 22:21:03 +0800

    v3。0提交

--- src/test/java/com/example/seckilldemo/SeckillDemoApplicationTests.java
+++ src/test/java/com/example/seckilldemo/SeckillDemoApplicationTests.java
@@ -22,8 +22,11 @@ class SeckillDemoApplicationTests {
     @Autowired
     private RedisTemplate redisTemplate;
     @Autowired
-    private RedisScript<Boolean> redisScript;
+    private RedisScript<Boolean> lockScript;
 
+    /**
+     * 问题：如果中间抛出异常，K1没有被删除，会导致死锁
+     */
     @Test
     public void testLock01() {
         ValueOperations valueOperations = redisTemplate.opsForValue();
@@ -40,6 +43,14 @@ class SeckillDemoApplicationTests {
         }
     }
 
+    /**
+     * 死锁问题解决：setIfAbsent配置超时时间：
+     *
+     * 问题：
+     *      第一个线程获取到锁然后超时锁不见了，线程还在继续执行（耗时操作）；
+     *      第二个线程进入上锁，
+     *      此时：加入第一个线程执行结束会删除锁，此时删掉的是第二个线程的锁。乱序问题。
+     */
     @Test
     public void testLock2() {
         ValueOperations valueOperations = redisTemplate.opsForValue();
@@ -56,6 +67,9 @@ class SeckillDemoApplicationTests {
         }
     }
 
+    /**
+     * 解决乱序问题：K1的值采用随机值，删除的时候根据随机值判断是不是自己的锁。
+     */
     @Test
     public void testLock3() {
         ValueOperations valueOperations = redisTemplate.opsForValue();
@@ -67,7 +81,7 @@ class SeckillDemoApplicationTests {
             System.out.println("name=" + name);
             //操作结束，删除锁
             System.out.println(valueOperations.get("k1"));
-            Boolean result = (Boolean) redisTemplate.execute(redisScript, Collections.singletonList("k1"), value);
+            Boolean result = (Boolean) redisTemplate.execute(lockScript, Collections.singletonList("k1"), value);
             System.out.println(result);
         } else {
             System.out.println("有线程在使用，请稍后再试");
